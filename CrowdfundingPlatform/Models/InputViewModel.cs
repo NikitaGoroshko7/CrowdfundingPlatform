b5f79b703f2850ed5d1f8fecc9b28f6534cc7ac8
NikitaGoroshko7 <nikita.goroshko7@gmail.com>
2022-01-12 16:57:38 +0300

    Создание моделей https://trello.com/c/iYe30mxs/3-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-%D0%BC%D0%BE%D0%B4%D0%B5%D0%BB%D0%B5%D0%B9 Added Models

---
+++ CrowdfundingPlatform/Models/InputViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrowdfundingPlatform.Models
+{
+    public class InputViewModel
+    {
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
